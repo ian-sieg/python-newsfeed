4deffd5ae824ffe96ac7e14c6ad50d2504a68032
ian.moss <ianmsieg@gmail.com>
2022-12-10 15:40:35 -0600

    working on hooking up flask server

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from .routes import home, dashboard
+from .db import init_db
 
 def create_app(test_config=None):
-  # set up app config
-  app = Flask(__name__, static_url_path='/')
-  app.url_map.strict_slashes = False
-  app.config.from_mapping(
-    SECRET_KEY='super_secret_key'
-  )
+    # set up app config
+    app = Flask(__name__, static_url_path='/')
+    app.url_map.strict_slashes = False
+    app.config.from_mapping(
+        SECRET_KEY='super_secret_key'
+    )
+    
+    app.register_blueprint(home)
+    app.register_blueprint(dashboard)
+    
+    init_db()
 
-  app.register_blueprint(home)
-  app.register_blueprint(dashboard)
-
-  return app
+    return app
