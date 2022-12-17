4990aae3041c8e19aec96bb850f033698ffd3d9b
ian.moss <ianmsieg@gmail.com>
2022-12-17 11:06:26 -0600

    troubleshooting existing user login issue

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, session
 from app.models import User
 from app.db import get_db
+import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -20,7 +21,35 @@ def signup():
         db.commit()
     
     except:
+        print(sys.exc_info()[0])
         db.rollback()
         return jsonify(message = 'Signup failed'), 500
     
+    session.clear()
+    session['user_id'] = newUser.id
+    session['loggedIn'] = True
+    
     return jsonify(id = newUser.id)
+
+@bp.route('/users/logout', methods=['POST'])
+def logout():
+    session.clear()
+    return '', 204
+
+@bp.route('/users/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    db = get_db()
+    
+    try:
+        user = db.query(User).filter(User.email == data['email']).one()
+        if user.verify_password(data['password']) == False:
+            return jsonify(message = 'Incorrect credentials'), 400
+        else:
+            session.clear()
+            session['user_id'] = user.id
+            session['logged_in'] = True
+            return jsonify(id = user.id)
+    except:
+        print(sys.exc_info()[0])
+        return jsonify(message = 'Incorrect credentials'), 400
